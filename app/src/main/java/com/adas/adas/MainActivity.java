a5cb59e7f4b22f1c74af2d9d57fb065f78111bcc
Drew DeVos <drew.devos@solidfire.com>
2016-07-14 14:30:48 -0600

    added opencv stuff

--- app/src/main/java/com/adas/adas/MainActivity.java
+++ app/src/main/java/com/adas/adas/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
     }
 }
