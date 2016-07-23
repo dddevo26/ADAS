31a37cb0ee4fe72023f196004e94b303232ad636
jtunnell <jordantunnell@gmail.com>
2016-07-22 19:48:09 -0600

    test button and basic git

--- app/src/main/java/com/adas/adas/MainActivity.java
+++ app/src/main/java/com/adas/adas/MainActivity.java
@@ -2,6 +2,12 @@ package com.adas.adas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!OpenCVLoader.initDebug()) {
-            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+            Log.i(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
         } else {
-            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+            Log.i(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
         }
+        Button btntest = (Button)(findViewById(R.id.btntest));
+        btntest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
