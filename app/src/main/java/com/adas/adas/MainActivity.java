541e11553477ed76c29a84338ea8e5f9f394c628
dddevo26 <dddevo26@rams.colostate.edu>
2016-08-21 23:16:33 -0600

    menu options

--- app/src/main/java/com/adas/adas/MainActivity.java
+++ app/src/main/java/com/adas/adas/MainActivity.java
@@ -4,6 +4,9 @@ import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
@@ -19,6 +22,51 @@ import org.opencv.core.Mat;
 
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.video:
+                checkItem(item);
+                return true;
+            case R.id.image:
+                checkItem(item);
+                return true;
+            case R.id.vehicle_detection:
+                checkItem(item);
+                return true;
+            case R.id.lane_detection:
+                checkItem(item);
+                return true;
+            case R.id.people_detection:
+                checkItem(item);
+                return true;
+            case R.id.color_conversion:
+                checkItem(item);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void checkItem(MenuItem item){
+        if (item.isChecked() == false)
+            item.setChecked(true);
+        else if(item.isChecked() == true)
+            item.setChecked(false);
+    }
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this){
         @Override
